f854cf8481f056487f873021b22e422698fdb2db
Zheng_Liu <tjuliuzh@gmail.com>
2019-09-27 22:24:44 -0400

    Refresher

--- Westin_Cincinnati.py
+++ Westin_Cincinnati.py
@@ -4,9 +4,9 @@
 
 import requests, bs4, os, time, re
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 
